6be1078e869343e615334da414f2b98379924f95
PhilipAtEmcogma <emcogma@gmail.com>
2023-01-25 01:07:11 +1300

    updated migration.js to automate some deployment

--- migrations/2_token_migration.js
+++ migrations/2_token_migration.js
@@ -1,6 +1,16 @@
 //remeber to use the name of the contract "Wallet" not the name of the js file "wallet"
 const Link = artifacts.require("Link");
+const Wallet = artifacts.require("Wallet");
 
-module.exports = function(deployer){
-    deployer.deploy(Link);
+//remeber in order to use "await", the function need to be async
+module.exports = async function(deployer){
+    await deployer.deploy(Link);
+
+    let wallet = await Wallet.deployed();
+    let link = await Link.deployed();
+
+    await link.approve(wallet.address, 500); //assigning 500 tokens to wallet as quota to use
+    wallet.addToken(web3.utils.fromUtf8("LINK"), link.address);
+    //fromUtf8 takes string input and returns its UTF-8 encoded version as bytes
+    await wallet.deposit(100, web3.utils.fromUtf8("LINK"));
 };
