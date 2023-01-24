640ae270a5b6a4c618d3f456478ffe5d71627b48
PhilipAtEmcogma <emcogma@gmail.com>
2023-01-24 18:31:03 +1300

    deposit function in wallet.sol added.

--- contracts/wallet.sol
+++ contracts/wallet.sol
@@ -3,8 +3,9 @@ pragma solidity 0.8.17;
 
 import "../node_modules/@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "../node_modules/@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
 
-contract Wallet{
+contract Wallet is Ownable{
     using SafeMath for uint256;
 
     //struct for storing token ticker and address
@@ -22,25 +23,31 @@ contract Wallet{
     //this mapping is used to store balances for all users
     mapping(address => mapping(bytes32 => uint256)) public balances;
 
-    function addToken(bytes32 ticker, address tokenAddress) external{
+    modifier tokenExist(bytes32 ticker){
+        //using require to check if ticker exist before proceeding with the deposit
+        //!= address(0), because is uninitialise token address (0x000000000000000), 
+        require(tokenMapping[ticker].tokenAddress != address(0), "Token Does Not Exist!");
+        _;
+    }
+
+    //onlyOwner is from Ownable.sol library, signifying only owner of wallet can addToken
+    function addToken(bytes32 ticker, address tokenAddress) onlyOwner external{
         tokenMapping[ticker] = Token(ticker, tokenAddress);
         tokenList.push(ticker);
     }
 
-    function deposit(uint amount, bytes32 ticker) external{
+    //tokenExist is a custom modifier used to check if a token exist
+    function deposit(uint amount, bytes32 ticker) tokenExist(ticker) external{
+        //transferFrom function is used to transfer from msg.sender to address(this)
+        IERC20(tokenMapping[ticker].tokenAddress).transferFrom(msg.sender, address(this), amount);
 
+        balances[msg.sender][ticker] = balances[msg.sender][ticker].add(amount);
     }
 
-    function withdraw(uint amount, bytes32 ticker) external{
-        //check if the token actually exist before processing the withdraw process
-        //!= address(0), because is uninitialise token address (0x000000000000000), 
-        require(tokenMapping[ticker].tokenAddress != address(0));        
-        
+    function withdraw(uint amount, bytes32 ticker) tokenExist(ticker) external{        
         //use require to make sure sender have enough amount of token to proceed withdraw
         //if insufficient, throw error message.  else proceed to the next step
         require(balances[msg.sender][ticker] >= amount, "Insufficient Balance");
-
-
 
         //.sub() is a Safe Math funrtion from openzepplin library (SafeMath.sol)
         //use to prevent possible overflow and underflow from occuring.
