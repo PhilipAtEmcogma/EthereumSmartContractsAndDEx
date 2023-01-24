d11e685792c79d6121498f2a6ae19b51a958c98e
PhilipAtEmcogma <emcogma@gmail.com>
2023-01-25 01:11:47 +1300

    added console log of new balance into migrate.js

--- migrations/2_token_migration.js
+++ migrations/2_token_migration.js
@@ -3,7 +3,7 @@ const Link = artifacts.require("Link");
 const Wallet = artifacts.require("Wallet");
 
 //remeber in order to use "await", the function need to be async
-module.exports = async function(deployer){
+module.exports = async function(deployer, network, accounts){
     await deployer.deploy(Link);
 
     let wallet = await Wallet.deployed();
@@ -13,4 +13,7 @@ module.exports = async function(deployer){
     wallet.addToken(web3.utils.fromUtf8("LINK"), link.address);
     //fromUtf8 takes string input and returns its UTF-8 encoded version as bytes
     await wallet.deposit(100, web3.utils.fromUtf8("LINK"));
+
+    let balanceOfLink = await wallet.balances(accounts[0], web3.utils.fromUtf8("LINK"));
+    console.log(balanceOfLink);
 };
