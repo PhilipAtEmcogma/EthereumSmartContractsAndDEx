aa4fa366aa2bb4fa573e56cf74e5cd1781a32c3d
PhilipAtEmcogma <emcogma@gmail.com>
2023-01-26 01:22:09 +1300

    update test cases

--- test/wallettest.js
+++ test/wallettest.js
@@ -19,8 +19,20 @@ contract("Dex", accounts => {
         let link = await Link.deployed();
         await link.approve(dex.address, 500);
         await dex.deposit(100, web3.utils.fromUtf8("LINK"));
-        let balance = await dex.balances(accounts[0], web3.utils.fromUtf8("LINK"))
+        let balance = await dex.balances(accounts[0], web3.utils.fromUtf8("LINK"));
         assert.equal(balance.toNumber(), 100);
+    })
+
+    it("should handle faulty withdrawals correctly", async() => {
+        let dex = await Dex.deployed();
+        let link = await Link.deployed();
+        await truffleAssert.reverts(dex.withdraw(500, web3.utils.fromUtf8("LINK")));
+    })
+
+    it("should handle correct withdrawals correctly", async() => {
+        let dex = await Dex.deployed();
+        let link = await Link.deployed();
+        await truffleAssert.passes(dex.withdraw(100, web3.utils.fromUtf8("LINK")));
     })
 
 })
