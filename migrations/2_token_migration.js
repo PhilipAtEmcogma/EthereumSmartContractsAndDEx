d7864c49f3ab01a43951f2079d24a16f262a0cab
PhilipAtEmcogma <emcogma@gmail.com>
2023-01-26 01:01:13 +1300

    updated token_migration.js. update test cases.

--- migrations/2_token_migration.js
+++ migrations/2_token_migration.js
@@ -3,17 +3,6 @@ const Link = artifacts.require("Link");
 const Dex = artifacts.require("Dex");
 
 //remeber in order to use "await", the function need to be async
-module.exports = async function(deployer, network, accounts){
+module.exports = async function(deployer, network, accounts) {
     await deployer.deploy(Link);
-
-    let dex = await Dex.deployed();
-    let link = await Link.deployed();
-
-    await link.approve(dex.address, 500); //assigning 500 tokens to wallet as quota to use
-    dex.addToken(web3.utils.fromUtf8("LINK"), link.address);
-    //fromUtf8 takes string input and returns its UTF-8 encoded version as bytes
-    await dex.deposit(100, web3.utils.fromUtf8("LINK"));
-
-    let balanceOfLink = await dex.balances(accounts[0], web3.utils.fromUtf8("LINK"));
-    console.log(balanceOfLink);
 };
--- test/wallettest.js
+++ test/wallettest.js
@@ -4,11 +4,23 @@ const truffleAssert = require('truffle-assertions');
 
 contract("Dex", accounts => {
     //tests are defined by the keyword "it"
-    it("should only be possible for owner to add tokens", async() =>{
+    it("should only be possible for owner to add tokens", async() => {
         let dex = await Dex.deployed();
         let link = await Link.deployed();
         await truffleAssert.passes(
             dex.addToken(web3.utils.fromUtf8("LINK"), link.address, {from: accounts[0]})
-            );
+        )
+        await truffleAssert.reverts(
+            dex.addToken(web3.utils.fromUtf8("LINK"), link.address, {from: accounts[1]})
+        )
     })
+    it("should handle deposits correctly", async() => {
+        let dex = await Dex.deployed();
+        let link = await Link.deployed();
+        await link.approve(dex.address, 500);
+        await dex.deposit(100, web3.utils.fromUtf8("LINK"));
+        let balance = await dex.balances(accounts[0], web3.utils.fromUtf8("LINK"))
+        assert.equal(balance.toNumber(), 100);
+    })
+
 })
